039133f3a19023a338867f9fb879ad99146bd247
lenovo <ntodor358@gmail.com>
2022-11-26 18:10:41 +0200

    add method for reading numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
-        restaurant.menu.printMenu();
-        restaurant.orderList.PrintOrderList();
         Login.printStartMenu(restaurant);
 
     }
